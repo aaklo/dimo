d9e875b7b086351f1b21308618cea3d75802ec00
zhangying <aaklo@126.com>
2020-04-21 16:07:04 +0800

    Initial commit

---
+++ ui/src/main/java/com/abc/ui/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.abc.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hhhhhhhhhhhhhh";
+    }
+}
